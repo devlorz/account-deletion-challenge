5141ff480d2a56ed9e9acf1b727f46750909d94e
Nutti Saelor <leelorz6@gmail.com>
2018-10-12 15:06:30 +0700

    add assertions to cypress test

--- client/cypress/e2e/transfer.spec.js
+++ client/cypress/e2e/transfer.spec.js
@@ -1,12 +1,14 @@
 describe('Transfer', () => {
   it('successful transfer', () => {
-    cy.visit('/')
+    cy.visit('/').wait(1000)
+    cy.getByText('Next').should('be.disabled')
     cy.get(':nth-child(1) > :nth-child(2) > div > select').select('Riker Lynch')
     cy.get(':nth-child(2) > :nth-child(2) > div > select').select(
       'Edward Bayer'
     )
     cy.getByText('Next').click()
 
+    cy.getByText('Next').should('be.disabled')
     cy.getByLabelText('I do not understand how to use this.').check()
     cy.getByLabelText('I do not need this.').check()
     cy.getByLabelText('I prefer other apps.').check()
@@ -14,10 +16,15 @@ describe('Transfer', () => {
     cy.get('textarea').type('other reason...')
     cy.getByText('Next').click()
 
+    cy.getByText('Delete my account').should('be.disabled')
     cy.getByPlaceholderText('ross@example.com').type('ross@example.com')
     cy.getByLabelText('I understand the consequences.').check()
-    cy.getByText('Delete my account').click()
+    cy.wait(5000)
+      .getByText('Delete my account')
+      .click()
 
-    // cy.getByText('Example Domain').should('exist')
+    cy.wait(5000)
+      .url()
+      .should('eq', 'http://www.example.com/')
   })
 })
