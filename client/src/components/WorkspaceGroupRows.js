5fb5b534416f4171353e28cfd397d6bf27ec275e
Nutti Saelor <leelorz6@gmail.com>
2018-10-12 15:58:34 +0700

    extract WorkspaceGroupRows component

---
+++ client/src/components/WorkspaceGroupRows.js
@@ -0,0 +1,31 @@
+import _ from 'lodash'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const WorkspaceGroupRows = props =>
+  !props.shouldDisplay ? null : (
+    <div>
+      <h3>{props.groupTitle}</h3>
+      <div>
+        {_.map(props.workspaces, workspace => (
+          <div key={workspace.spaceId} style={{ marginTop: '1rem' }}>
+            <span>Workspace: {workspace.displayName}</span>
+            <span>
+              {React.Children.count(props.children) === 0
+                ? null
+                : React.cloneElement(props.children, { workspace })}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+
+WorkspaceGroupRows.propTypes = {
+  groupTitle: PropTypes.string,
+  workspaces: PropTypes.array.isRequired,
+  children: PropTypes.node,
+  shouldDisplay: PropTypes.bool,
+}
+
+export default WorkspaceGroupRows
